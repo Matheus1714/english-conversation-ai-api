7f9e0c40c88eaf8fddfcb724528393851ec70a2d
matheus1714 <matheussilvamartins1714@gmail.com>
2023-12-20 16:14:57 -0300

    create log file configuration

---
+++ log.py
@@ -0,0 +1,23 @@
+import logging
+
+def get_log(log_file: str = 'debug') -> logging.Logger:
+
+    logging.getLogger().handlers = []
+    logging.basicConfig(level=logging.INFO)
+
+    log = logging.getLogger(log_file)
+
+    console_handler = logging.StreamHandler()
+    console_formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    console_handler.setFormatter(console_formatter)
+
+    file_handler = logging.FileHandler(log_file)
+    file_formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
+    file_handler.setFormatter(file_formatter)
+
+    log.addHandler(console_handler)
+    log.addHandler(file_handler)
+
+    log.info('Start Log...')
+
+    return log
