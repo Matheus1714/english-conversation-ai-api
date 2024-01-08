a01d6bad534381a7fbe9aedf463229ff2a626932
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-08 20:39:31 -0300

    update app with params

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import os
 
 from dotenv import dotenv_values
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, make_response
 from flask_cors import CORS
 from openai import OpenAI
 
 from utils import *
 
-# -- Local --
-config = dotenv_values(".env")
-OPEN_API_KEY = config.get('OPEN_API_KEY')
+from log import get_log
+from utils import *
 
-# -- Production --
-# OPEN_API_KEY = os.environ.get('OPEN_API_KEY', '')
+config = dotenv_values('.env')
+OPEN_API_KEY = config.get('OPEN_API_KEY')
 
 app = Flask(__name__)
 CORS(app)
+
+log = get_log('english_conversation_ai_api.log')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -26,23 +27,34 @@ def upload_audio():
     try:
         if 'audio' not in request.files:
             msg = 'Nenhum arquivo de áudio enviado'
+            log.info(msg)
             return jsonify({'error': msg}), 400
 
         audio_file = request.files['audio']
         audio_path = save_audio_file(audio_file, 'input.wav')
 
+        gpt_params = {}
+        gpt_params['language'] = request.form['language']
+        gpt_params['model'] = request.form['model']
+        gpt_params['temperature'] = float(request.form['temperature'])
+        gpt_params['voice'] = request.form['voice']
+
+        log.info(gpt_params)
+
         client = OpenAI(api_key=OPEN_API_KEY)
 
         transcript = transcribe_audio(client, audio_path)
-        content = chat_with_gpt(client, transcript)
-        response = generate_audio_response(client, content)
+        content = chat_with_gpt(client, transcript, **gpt_params)
+        response = generate_audio_response(client, content, **gpt_params)
 
         speech_path = os.path.join(upload_folder, 'output.wav')
         response.stream_to_file(speech_path)
 
+        log.info(f'Arquivo criado com sucesso!')
         return send_file(speech_path, as_attachment=True)
 
     except Exception as e:
+        log.exception(e)
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
